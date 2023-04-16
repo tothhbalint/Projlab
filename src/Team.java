cc7ec9d2d0d29480991effaef6972a2c3bb2b114
Gergo Buzas <buzasgergo0615@gmail.com>
2023-04-16 18:26:40 +0200

    Team skeleton done

--- src/Team.java
+++ src/Team.java
@@ -10,17 +10,21 @@
 public class Team {
 	/** */
 	public void addPoints(int i) {
+		System.out.println("Team: addPoints()");
 	}
 	
 	/** */
 	public void addMember(Player p) {
+		System.out.println("Team: addMember()");
 	}
 	
 	/** */
 	public void createPlumberTeam(String name, int nop) {
+		System.out.println("Team: createPlumberTeam()");
 	}
 	
 	/** */
 	public void createNomadTeam(String name, int nop) {
+		System.out.println("Team: createNomadTeam()");
 	}
 }
