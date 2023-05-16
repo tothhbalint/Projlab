8ca1335f702ee5a41a820718b21fb6120f492744
Tóth Bálint <toth3balint@gmail.com>
2023-05-16 22:48:01 +0200

    7-10 geroke

--- src/test.sh
+++ src/test.sh
@@ -7,17 +7,17 @@ eval javac *.java
 while :; do
   echo "Choose which test to run
         \n      0. Run all tests
-        \n      1. Movement()
-        \n      2. Movement(occupied pipe)
-        \n      2. Movement(slippy pipe)
-        \n      3. Movement(sticky pipe)
+        \n      1. Movement() #done
+        \n      2. Movement(occupied pipe) #done
+        \n      3. Movement(slippy pipe)
+        \n      4. Movement(sticky pipe)
         \n      5. Pump pickup
         \n      6. Pipe pickup
         \n      7. Pump placement
         \n      8. Break pipe
         \n      9. Fix
         \n      10. Connect pipe
-        \n      13. Water flow"
+        \n      11. Water flow" #done
 
   read input
   #add grep at the end of the lines, compare with outputs from a file
@@ -26,7 +26,7 @@ while :; do
     java Proto -t step -plumber0 -pipe0 >test1.txt
     cat test1.txt
     grep "player accepted" <test1.txt
-    if test "$(wc -l <test1.txt)" -eq 1; then
+    if test "$(grep "player accepted" <test1.txt | wc -l )" -eq 1; then
       echo "Success"
     else
       echo "Failed"
@@ -56,13 +56,13 @@ while :; do
   7) echo "fix -player1" | $PROTO ;;
   8) echo "place -plumber1 -pump" | $PROTO ;;
   9) echo "oil -nomad1" | $PROTO ;;
-  11) echo "glue -player1" | $PROTO ;;
-  12)
-    java Proto -t break -nomad0 flow>test13.txt
+  10) echo "glue -player1" | $PROTO ;;
+  11)
+    java Proto -t flow>test13.txt
     cat test13.txt
     grep "Water state set to true" test13.txt
     grep "cistern.receiveWater" test13.txt
-    if test "$(grep "Water state set to true" test13.txt | wc -l)" -eq 8 &&
+    if test "$(grep "Water state set to true" test13.txt | wc -l)" -eq 9 &&
        test "$(grep "cistern.receiveWater" test13.txt | wc -l)" -eq 3; then
       echo "Success"
     else
