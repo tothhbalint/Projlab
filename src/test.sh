a0a7bcec6ac7adcd8b80a5d60ec6e2c60fdaa138
Gergo Buzas <buzasgergo0615@gmail.com>
2023-05-17 02:08:44 +0200

    Additional tests added

--- src/test.sh
+++ src/test.sh
@@ -7,24 +7,36 @@ eval javac *.java
 while :; do
   echo "Choose which test to run
         \n      0. Run all tests
-        \n      1. Movement() #done
+        \n      1. Movement(pipe) #done
         \n      2. Movement(occupied pipe) #done
-        \n      3. Movement(slippy pipe)
-        \n      4. Movement(sticky pipe)
-        \n      5. Pump pickup
-        \n      6. Pipe pickup
-        \n      7. Pump placement
-        \n      8. Break pipe
-        \n      9. Fix
-        \n      10. Connect pipe
-        \n      11. Water flow
-        \n      12. Points added to plumber team
-        \n      13. Points added to nomad team" #done
+        \n      3. Movement (pump)
+        \n      4. Movement (cistern)
+        \n      5. Movement (source)
+        \n      6. Slippery pipe
+        \n      7. Sticky pipe
+        \n      8. Slippery pipe on sticky pipe
+        \n      9. Movement(slippery pipe)
+        \n      10. Movement(sticky pipe)
+        \n      11. Pump pickup
+        \n      12. Pump pickup at invalid location
+        \n      13. Pipe pickup
+        \n      14. Inventory is full (Pipe)
+        \n      15. Inventory is full (Pump)
+        \n      16. Pump placement
+        \n      17. Break pipe
+        \n      18. Fix pipe
+        \n      19. Connect pipe
+        \n      20. Water flows from cistern to pipe
+        \n      21. Water flows from pipe to Pump
+        \n      22. Water flows from pump to pipe
+        \n      23. Water flows from pipe to cistern
+        \n      24. Points added to plumber team
+        \n      25. Points added to nomad team"
 
   read input
   #add grep at the end of the lines, compare with outputs from a file
   case $input in
-  1)
+  1) #Movement (pipe)
     java Proto -t step -plumber0 -pipe0 >test1.txt
     cat test1.txt; echo " ";
     grep "player accepted" <test1.txt
@@ -34,7 +46,8 @@ while :; do
       echo "Failed"
     fi
     ;;
-  2) java Proto -t step -plumber1 -pump0 >test2.txt
+  2) #Movement (occupied pipe)
+    java Proto -t step -plumber1 -pump0 >test2.txt
     cat test2.txt; echo " ";
     grep "player rejected" <test2.txt
     grep "pipe occupied" <test2.txt
@@ -44,7 +57,27 @@ while :; do
     else
       echo "Failed"
     fi ;;
-  3)
+  3) #Movement(pump)
+        java Proto -t step -plumber0 -pump0 >test1.txt
+        cat test1.txt; echo " ";
+        grep "player accepted" <test1.txt
+        if test "$(grep "player accepted" <test1.txt | wc -l )" -eq 1; then
+          echo "Success"
+        else
+          echo "Failed"
+        fi
+        ;;
+  4) #Movement(cistern)
+    ;;
+  5) #Movement(source)
+    ;;
+  6) #Slippery pipe
+    ;;
+  7) #Sticky pipe
+    ;;
+  8) #Slippery pipe on sticky pipe
+    ;;
+  9) # Movement(slippery pipe)
     java Proto -t oil -nomad1 step -nomad1 -p1 step -plumber1 -p0 >test3.txt
     cat test3.txt; echo " ";
     grep "player slipped" <test3.txt
@@ -56,7 +89,7 @@ while :; do
     else
       echo "Failed"
     fi ;;
-  4)
+  10) #Movement(sticky pipe)
     java Proto -t glue -nomad1 step -nomad1 -p1 step -plumber1 -p0 step -plumber1 -p0 >test4.txt
     cat test4.txt; echo " ";
     grep "player is stuck" <test4.txt
@@ -67,7 +100,7 @@ while :; do
     else
       echo "Failed"
     fi ;;
-  5)
+  11) #Pump pickup
     java Proto -t pickup -plumber0 -pump step -plumber0 -p0 step -plumber0 -p1 step -plumber0 -p1 step -plumber0 -p1 step -plumber0 -p1 step -plumber0 -p1 pickup -plumber0 -pump>test5.txt
     cat test5.txt; echo " ";
     grep "pickup failed" <test5.txt
@@ -78,7 +111,16 @@ while :; do
     else
       echo "Failed"
     fi ;;
-  6) #pipe pickup
+  12) #Pump pickup at invalid location
+    java Proto -t pickup -plumber0 -pump > test5.txt
+    cat test5.txt; echo " ";
+    grep "pickup failed" <test5.txt
+    if test "$(grep "pickup failed" <test5.txt | wc -l)" -eq 1; then
+      echo "Success"
+    else
+      echo "Failed"
+    fi ;;
+  13) #pipe pickup
     java Proto -t step -plumber0 -p0 step -plumber0 -p1 pickup -plumber0 -pipe
     cat test6.txt; echo " ";
     grep "pipe added to the inventory" <test6.txt
@@ -88,7 +130,11 @@ while :; do
       echo "Failed"
     fi
     ;;
-  7) #pump placement
+  14) #Inventory is full (pipe)
+    ;;
+  15) #Inventory is full (pump)
+    ;;
+  16) #pump placement
     java Proto -t step -plumber0 -p0 step -plumber0 -p1 step -plumber0 -p1 step -plumber0 -p1 step -plumber0 -p1 step -plumber0 -p1 pickup -plumber0 -pump step -plumber0 -p0 place -plumber0 -pump >test7.txt
     cat test7.txt; echo " ";
     grep "pump added to the inventory" <test7.txt
@@ -100,7 +146,7 @@ while :; do
       echo "Failed"
     fi
     ;;
-  8) #break
+  17) #break pipe
     java Proto -t break -nomad1 > test8.txt
     cat test8.txt; echo " ";
     grep "pipe broken" <test8.txt
@@ -110,7 +156,7 @@ while :; do
       echo "Failed"
     fi
     ;;
-  9) #fix
+  18) #fix pipe
     java Proto -t break -nomad1 step -nomad1 -p0 step -plumber0 -p0 step -plumber0 -p1 step -plumber0 -p2 step -plumber0 -p1 step -plumber0 -p0 fix -plumber0 >test9.txt
     cat test9.txt; echo " ";
     if test "$(grep "pipe fixed" <test9.txt | wc -l)" -eq 1 &&
@@ -120,7 +166,9 @@ while :; do
       echo "Failed"
     fi
     ;;
-  11) #water transfer
+  19) #Connect pipe
+    ;;
+  20) # Water flows from cistern to pipe
     java Proto -t flow>test13.txt
     cat test13.txt
     grep "Water state set to true" test13.txt
@@ -132,7 +180,13 @@ while :; do
       echo "Failed"
     fi
     ;;
-  12) #Points added to Plumber's score
+  21) # Water flows from pipe to pump
+     ;;
+  22) # Water flows from pump to pipe
+    ;;
+  23) # Water flows from pipe to cistern
+    ;;
+  24) #Points added to Plumber's score
     java Proto -t flow flow flow flow flow flow >test12.txt # 6 flow should transfer the water to the cistern
     cat test12.txt
     grep "Plumber's score increased" test12.txt
@@ -143,7 +197,7 @@ while :; do
     fi
 
     ;;
-  13) #Points added to Nomad's score
+  25) #Points added to Nomad's score
     # Here I wanted to break the first pipe, at bottom-left.
     # That should add 5 points to Nomad's score (1: transfer water to pipe, 2-6: points added)
     # The other two cisterns should get 1-1 point each.
