c9b70c43b5abe78d8cf5c28efe08c54d6e4a4c30
Tóth Bálint <toth3balint@gmail.com>
2023-05-18 20:13:10 +0200

    jplumberxdxxd

--- src/View/JPlumber.java
+++ src/View/JPlumber.java
@@ -1,6 +1,32 @@
 package View;
-import Model.*;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
-public class JPlumber extends JPanel {
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class JPlumber   {
+    private int x,y;
+
+    private final BufferedImage plumberImage;
+
+    public JPlumber(int X, int Y) throws IOException {
+        x = X;
+        y = Y;
+
+        File imagefile = new File("./src/View/plumber.png");
+         plumberImage = ImageIO.read(imagefile);
+
+    }
+
+    public Image getImage(){
+        return plumberImage;
+    }
+
+    public void move(int X, int Y){
+       x = X;
+       y = Y;
+    }
 }
