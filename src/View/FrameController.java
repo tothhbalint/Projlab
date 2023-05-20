80b2c4602a8db806a58acf10b0b1144b0076556c
Tóth Bálint <toth3balint@gmail.com>
2023-05-21 01:03:52 +0200

    elbasztam xd

---
+++ src/View/FrameController.java
@@ -0,0 +1,8 @@
+package View;
+
+public class FrameController {
+    public static void main(String[] args) {
+        MenuFrame menuFrame = new MenuFrame();
+        GameFrame gameFrame = new GameFrame();
+    }
+}
---
+++ src/View/JPlayer.java
@@ -0,0 +1,8 @@
+package View;
+
+public abstract class JPlayer extends JGameElement{
+    public JPlayer(int _x, int _y) {
+        super(_x, _y);
+    }
+
+}
